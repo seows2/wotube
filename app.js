55badfebbf8b0f52ba63705c760a0a057384f595
서우석 <seows@seouseog-ui-MacBook-Pro.local>
2020-01-08 23:32:02 +0900

    comiit

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
 import globalRouter from "./routers/globalRouter"
 import routes from "./routes"
-
+import { localMiddleware } from "./middlewares"
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookie_parser());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localMiddleware);
 
 app.use(routes.home,globalRouter);
 app.use(routes.users, userRouter);
