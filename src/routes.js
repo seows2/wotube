78ea0bde378ce7f2e4679f80d70bf0727fb9a57c
SeoWS <seows2@naver.com>
2020-05-19 17:31:54 +0900

    README update

--- src/routes.js
+++ src/routes.js
@@ -35,82 +35,75 @@ const FACEBOOK_CALLBACK = "/auth/facebook/callback";
 const KAKAO = "/auth/kakao";
 const KAKAO_CALLBACK = "/auth/kakao/callback";
 
-
-//메인 로고 URL 
-const IMG = "/img"
-
 //API
 
-const API = "/api"
-const REGISTER_VIEW = "/:id/view"   //비디오 아이디
-const ADD_COMMENT = "/:id/comment"
-const DELETE_COMMENT = "/:id/delete"
-
+const API = "/api";
+const REGISTER_VIEW = "/:id/view"; //비디오 아이디
+const ADD_COMMENT = "/:id/comment";
+const DELETE_COMMENT = "/:id/delete";
 
 const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: (id) => {
-        if (id) {
-            return `/users/${id}`;
-        } else {
-            return USER_DETAIL;
-        }
-    },
-    editProfile: (id) => {
-        if (id) {
-            return `/users${EDIT_PROFILE}`;
-        } else {
-            return EDIT_PROFILE;
-        }
-    },
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEOS,
-    upload: UPLOAD,
-    videoDetail: (id) => {
-        if (id) {
-            return `/videos/${id}`;
-        } else {
-            return VIDEO_DETAIL;
-        }
-    },
-    editVideo: (id) => {
-        if (id) {
-            return `/videos/${id}/edit`;
-        } else {
-            return EDIT_VIDEO;
-        }
-    },
-    deleteVideo: (id) => {
-        if (id) {
-            return `/videos/${id}/delete`;
-        } else {
-            return DELETE_VIDEO;
-        }
-    },
-    github: GITHUB,
-    githubCallback: GITHUB_CALLBACK,
-    facebook : FACEBOOK,
-    facebookCallback : FACEBOOK_CALLBACK,
-    kakao : KAKAO,
-    kakaoCallback : KAKAO_CALLBACK,
-    myPage: MYPAGE,
-    img : IMG,
-    api : API,
-    registerView : REGISTER_VIEW,
-    addComment : ADD_COMMENT,
-    deleteComment: (id) => {
-        if (id) {
-            return `/api/${id}/delete`;
-        } else {
-            return DELETE_COMMENT;
-        }
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
     }
-
+  },
+  editProfile: (id) => {
+    if (id) {
+      return `/users${EDIT_PROFILE}`;
+    } else {
+      return EDIT_PROFILE;
+    }
+  },
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
+  github: GITHUB,
+  githubCallback: GITHUB_CALLBACK,
+  facebook: FACEBOOK,
+  facebookCallback: FACEBOOK_CALLBACK,
+  kakao: KAKAO,
+  kakaoCallback: KAKAO_CALLBACK,
+  myPage: MYPAGE,
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
+  deleteComment: (id) => {
+    if (id) {
+      return `/api/${id}/delete`;
+    } else {
+      return DELETE_COMMENT;
+    }
+  },
 };
 
 export default routes;
